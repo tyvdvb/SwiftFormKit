671c1eda6d4694ffa4ebeb4fbafc99c258f5037e
tyvdvb <ira1980nazar@gmail.com>
2024-11-13 16:43:47 -0500

    Initial Commit

---
+++ SwiftFormKit/SwiftFormKit.h
@@ -0,0 +1,18 @@
+//
+//  SwiftFormKit.h
+//  SwiftFormKit
+//
+//  Created by Ira Nazar on 2024-11-13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftFormKit.
+FOUNDATION_EXPORT double SwiftFormKitVersionNumber;
+
+//! Project version string for SwiftFormKit.
+FOUNDATION_EXPORT const unsigned char SwiftFormKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFormKit/PublicHeader.h>
+
+
